7c3b3bec909cf3ff150eb678769bd98ce2e10e83
k-tsurumaki <92656989+k-tsurumaki@users.noreply.github.com>
2023-07-05 23:27:52 +0900

    material-uiのインストール

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         {/* Twitterアイコン */}
+        <TwitterIcon className='sidebar_twitterIcon'/>
         {/* Sidebarオプション */}
         {/* Tweetボタン */}
     </div>
